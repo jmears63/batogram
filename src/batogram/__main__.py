d99da0a1f24a9ae52836b47bae61e1d3fd92f16f
John Mears <john@themears.co.uk>
2023-04-08 16:24:22 +0100

    Release 1.0.5.

--- src/batogram/__main__.py
+++ src/batogram/__main__.py
@@ -1,8 +1,17 @@
+import argparse
+
 from . import rootwindow, constants as c
+from .constants import PROGRAM_NAME
 
 
 def run():
-    app = rootwindow.RootWindow(className=c.PROGRAM_NAME)
+    parser = argparse.ArgumentParser(
+        prog=PROGRAM_NAME,
+        description='This program displays spectograms from raw audio data.')
+    parser.add_argument('datafile', metavar='datafile', nargs='?', help='a .wav file contain raw audio data')
+    args = parser.parse_args()
+
+    app = rootwindow.RootWindow(className=c.PROGRAM_NAME, initialfile=args.datafile)
     app.mainloop()
 
 
