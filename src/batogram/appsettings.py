b7c3fd3de3957760403fa1d8fa514be41583994b
John Mears <john@themears.co.uk>
2023-04-10 21:56:18 +0100

    Settings modal WIP: data directory path is now used.
    Former-commit-id: 035f451cdd45a6a924e73c1d5688b72591b4cc15 [formerly 646fd31154e9a0dbe34e6fd0eeb8aedcbc43f27b]
    Former-commit-id: 67756ec1895fc5572959e1827800ec4023dce979

--- src/batogram/appsettings.py
+++ src/batogram/appsettings.py
@@ -1,5 +1,7 @@
 import os
 from dataclasses import dataclass
+from pathlib import Path
+
 from dataclasses_json import Undefined, dataclass_json
 from platformdirs import user_data_dir
 
@@ -9,7 +11,8 @@ from platformdirs import user_data_dir
 @dataclass()
 class AppSettings:
     colour_scale: str = "kindlmann-table-byte-1024.csv"
-    data_path: str = "."
+    data_directory: str = Path.home()
+    # Remember to add new attributes to the _copy_other method.
 
     def write(self):
         path = self._get_file_path()
@@ -23,7 +26,10 @@ class AppSettings:
             path = self._get_file_path()
             with open(path, "r") as f:
                 s = f.read()
-                self.from_json(s)
+                file_settings = self.from_json(s)
+                # Surely there is a neater way to do this?
+                self._copy_other(file_settings)
+
         except FileNotFoundError:
             pass    # There will be no such file the first time around.
 
@@ -32,4 +38,9 @@ class AppSettings:
         # Hard coded values so there are less likely to change by accident:
         d = user_data_dir("batogram", "fitzharrys")
         return "{}/appsettings.py".format(d)
+
+    def _copy_other(self, other: "AppSettings"):
+        self.colour_scale = other.colour_scale
+        self.data_directory = other.data_directory
+
 
